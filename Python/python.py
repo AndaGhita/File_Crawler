ca1f312669e63a63db493d45a389928985f1b3ca
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2023-03-10 10:17:53 +0100

    Update python.py

--- Python/python.py
+++ Python/python.py
@@ -2,19 +2,26 @@ import os
 
 def index_directory(directory):
     index = {}
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            filepath = os.path.join(root, file)
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory,item)
+        if os.path.isfile(item_path):
             try:
-                with open(filepath, 'r', encoding='utf-8') as f:
+                with open(item_path, 'r', encoding='utf-8') as f:
                     words = f.read().split()
                     for word in words:
                         if word in index:
-                            index[word].append(filepath)
+                            index[word].append(item_path)
                         else:
-                            index[word] = [filepath]
+                            index[word] = [item_path]
             except:
-                print(f"Could not read file: {filepath}")
+                print(f"Could not read file: {item_path}")
+        elif os.path.isdir(item_path):
+            sub_index = index_directory(item_path)
+            for word in sub_index:
+                if word in index:
+                    index[word] += sub_index[word]
+                else:
+                    index[word] = sub_index[word]
     return index
 
 def search_index(keyword, index):
